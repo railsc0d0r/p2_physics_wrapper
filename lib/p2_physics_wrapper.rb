6e41196136ad6a30fd88970d3c8d9b7f190fb17d
stephan <railsc0d0r@gmail.com>
2016-04-23 00:08:44 +0200

    Adds metaprogramming-stuff for models that use our gem. Refs #66, #92

--- lib/p2_physics_wrapper.rb
+++ lib/p2_physics_wrapper.rb
@@ -1,5 +1,25 @@
 require "p2_physics_wrapper/version"
 
 module P2PhysicsWrapper
-  # Your code goes here...
+
+  # Extends base w/ ClassMethods, when included
+  def self.included(base)
+    base.send :extend, ClassMethods
+  end
+
+  # Class-methods for models that use p2_physics
+  module ClassMethods
+  end
+
+  # Instance-methods for models that use p2_physics
+  module InstanceMethods
+    def method_missing(meth, *args)
+      if self.class.respond_to?(meth)
+        self.class.send(meth, *args)
+      else
+        raise NoMethodError.new "method '#{meth}' undefined for class '#{self.class}' or its instances"
+      end
+    end
+  end
+
 end
