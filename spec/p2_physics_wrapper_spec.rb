dcb4edca94a73a9ab63da122d072df7ff7f66da1
stephan <railsc0d0r@gmail.com>
2016-04-23 12:08:53 +0200

    Removes useless spec.

--- spec/p2_physics_wrapper_spec.rb
+++ spec/p2_physics_wrapper_spec.rb
@@ -4,8 +4,4 @@ describe P2PhysicsWrapper do
   it 'has a version number' do
     expect(P2PhysicsWrapper::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
