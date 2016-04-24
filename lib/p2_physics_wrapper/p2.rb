58f1770d2620612538ad0bf8f5c0a6d84d026b29
stephan <railsc0d0r@gmail.com>
2016-04-24 02:33:52 +0200

    Fixes loading of p2.js in rails-project. Refs #66, #92

--- lib/p2_physics_wrapper/p2.rb
+++ lib/p2_physics_wrapper/p2.rb
@@ -3,8 +3,8 @@ require 'v8'
 module P2PhysicsWrapper::P2
   @ctx = V8::Context.new
   @ctx['window'] = {}
-  gem_root = FileUtils.pwd
-  @ctx.load(gem_root + '/lib/p2_physics_wrapper/p2.js')
+  path = File.dirname __dir__
+  @ctx.load(path + '/p2_physics_wrapper/p2.js')
   @p2 = @ctx['window']['p2']
 
   class << self
